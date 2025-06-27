b75d1ee3c437963f66766ac485392bffbc034afe
ehseezed <97066152+ehseezed@users.noreply.github.com>
2025-06-26 19:57:25 -0500

    Initial

---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,29 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "consumable" but feel free to expand for your needs!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+ITEM_MAPPING = {
+    [400000] = {"sword", "progressive"},
+    [400001] = {"armor", "progressive"},
+    [400002] = {"shield", "progressive"},
+    [400003] = {"springelixir", "toggle"},
+    [400004] = {"Mattock", "toggle"},
+    [400005] = {"Wingboots", "toggle"},
+    [400006] = {"KeyJack", "toggle"},
+    [400007] = {"KeyQueen", "toggle"},
+    [400008] = {"KeyKing", "toggle"},
+    [400009] = {"KeyJoker", "toggle"},
+    [400010] = {"KeyAce", "toggle"},
+    [400011] = {"RingRuby", "toggle"},
+    [400012] = {"RingDworf", "toggle"},
+    [400013] = {"Demonsring", "toggle"},
+    [400014] = {"BlackOnyx", "toggle"},
+    [400015] = {"Deluge", "toggle"},
+    [400016] = {"Thunder", "toggle"},
+    [400017] = {"Fire", "toggle"},
+    [400018] = {"Death", "toggle"},
+    [400019] = {"Tilte", "toggle"},
+    [400020] = {"RingElf", "toggle"},
+    [400021] = {"MagicalRod", "toggle"},
+    [400022] = {"Pendant", "toggle"},
+    [400024] = {"RedPotion", "consumable"},
+}
